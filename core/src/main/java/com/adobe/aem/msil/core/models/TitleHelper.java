3bc7aab0b9ffb1d205ff927915a4bba2673fc212
Learning AEM <b6aemdeveloper@gmail.com>
2024-01-02 10:30:27 +0530

    Title Helper

--- core/src/main/java/com/adobe/aem/msil/core/models/TitleHelper.java
+++ core/src/main/java/com/adobe/aem/msil/core/models/TitleHelper.java
@@ -1,16 +1,23 @@
 package com.adobe.aem.msil.core.models;
 
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.RequestAttribute;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 
+import com.adobe.granite.asset.api.AssetManager;
+import com.adobe.granite.asset.api.AssetMetadata;
+import com.day.cq.dam.api.Asset;
 import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
 
 import lombok.Getter;
 
@@ -21,20 +28,34 @@ public class TitleHelper {
 	@RequestAttribute
 	private String pageUrl;
 	
+	
+	
 	private String title;
 	
 	@SlingObject
 	ResourceResolver rr;
 	
+	@SlingObject
+	PageManager pm;
+	
 	@PostConstruct
 	public void init()
 	{
 		
+		//Title using Resource and Page Adapting
+		
 		Resource pagePathResource = rr.getResource(pageUrl);
-		
 		Page pageProvided = pagePathResource.adaptTo(Page.class);
-		title = pageProvided.getPageTitle();
+		String pagetitleusingResource = pageProvided.getTitle();
+	
+		// Title USing PageManager and ValueMap
+
+		PageManager pm = pagePathResource.getResourceResolver().adaptTo(PageManager.class);
+		Page containingPage = pm.getContainingPage(pagePathResource);
+		ValueMap pageProperties = containingPage.getProperties();
+		String pageTitleusingValueMap = pageProperties.get("pagetitle",String.class);
 		
+		title = pagetitleusingResource != null?pagetitleusingResource:pageTitleusingValueMap;
 		
 	}
 
